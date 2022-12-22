39fd06c94501ec7b2b121d5b07baf2c953dae3eb
Anmol Gupta <anmolgupta@Anmols-MacBook-Air.local>
2022-12-23 03:04:45 +0530

    Added comments and posts delete feature

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
-const Post = require('../models/post')
+const Post = require('../models/post');
+const comment=require('../models/comment');
 
 module.exports.create = function(req, res){
     Post.create({
@@ -11,3 +12,18 @@ module.exports.create = function(req, res){
         return res.redirect('back');
     });
 }  
+
+module.exports.destroy=function(req,res){
+    //(err,post) is for if we het a post
+    Post.findById(req.params.id,function(err,post){
+        //.id means converting the object id to strings
+        if(post.user==req.user.id){
+            post.remove();
+            comment.deleteMany({post:req.params.id},function(err){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    });
+}
--- routes/posts.js
+++ routes/posts.js
@@ -7,6 +7,7 @@ const postsController=require('../controllers/posts_controller');
 //form is going to be submitted
 //passport.checkAuthentication is to make sure that only signed in user can post
 router.post('/create',passport.checkAuthentication,postsController.create);
+router.get('/destroy/:id',passport.checkAuthentication,postsController.destroy);
 
 module.exports=router;
 
