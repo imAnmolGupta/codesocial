99acc87ea55b610ebe1b28528de2e72a6e5d88b7
Anmol Gupta <anmolgupta@Anmols-MacBook-Air.local>
2023-02-09 03:00:05 +0530

    added social authentication using google oauth

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,8 @@
 // const { use } = require('passport');
 const User=require('../models/user');
+const path=require('path');
+//file system module for deletion of avatars
+const fs=require('fs');
 
 module.exports.profile = function(req, res){
     User.findById(req.params.id,function(err,user){
@@ -30,6 +33,12 @@ module.exports.update = async function(req, res){
                 user.email=req.body.email;
 
                 if(req.file){ 
+
+                    if(user.avatar){
+                        fs.unlinkSync(path.join(__dirname,'..',user.avatar));
+                    }
+
+
                     //this is saving the path of the uploaded file into the avatar field in the user
                     user.avatar = User.avatarPath + '/' + req.file.filename;
                     
